bc9ccb4fa773b0ea05e83fbdcb6eb4bc6a54f31a
jjcandido <jaelson.jr@gmail.com>
2024-10-25 01:01:53 -0300

    Inclusion of item in DB Online

--- rc_cardapio.py
+++ rc_cardapio.py
@@ -1,17 +1,16 @@
 import mysql.connector
-
-
 config = {
-    'user': 'root',
-    'host': 'localhost',
-    'port': 3306,
-    'database': 'cardapio'
+    'database': 'skipcr49_rc_cardapio',
+    'user': 'skipcr49_junior',
+    'password': 'Junior@Skip#24',
+    'host': '192.185.211.8',
+    'port': '3306'
 }
 
 def create_produto (prod_name,prod_price,categoria):
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
-    cursor.execute("INSERT INTO db_cardapio(prod_name, prod_price, categoria) VALUES(%s,%s,%s)",(prod_name,prod_price,categoria))
+    cursor.execute("INSERT INTO rc_cardapio(prod_name, prod_price, categoria) VALUES(%s,%s,%s)",(prod_name,prod_price,categoria))
     connection.commit()
     cursor.close()
     connection.close()
@@ -21,16 +20,16 @@ def read_produtos(pagina,limite):
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
     offset = (pagina - 1) * limite
-    cursor.execute("SELECT * FROM db_cardapio LIMIT %s OFFSET %s", (limite, offset))
-    db_cardapio = cursor.fetchall()
+    cursor.execute("SELECT * FROM rc_cardapio LIMIT %s OFFSET %s", (limite, offset))
+    rc_cardapio = cursor.fetchall()
     cursor.close()
     connection.close()
-    return db_cardapio
+    return rc_cardapio
 
 def update_produto(id, prod_name,prod_price,categoria):
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
-    cursor.execute("UPDATE db_cardapio SET prod_name = %s, prod_price = %s, categoria = %s WHERE id = %s", (prod_name, prod_price, categoria,id))
+    cursor.execute("UPDATE rc_cardapio SET prod_name = %s, prod_price = %s, categoria = %s WHERE id = %s", (prod_name, prod_price, categoria,id))
     connection.commit()
     cursor.close()
     connection.close()
@@ -39,12 +38,13 @@ def update_produto(id, prod_name,prod_price,categoria):
 def delete_produto(id):
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
-    cursor.execute("DELETE FROM db_cardapio WHERE id = %s", (id))
-    connection.commit
-    connection.close
+    cursor.execute("DELETE FROM rc_cardapio WHERE id = %s", (id))
+    connection.commit()
+    connection.close()
     print("Produto deletado com sucesso.")
 
 if __name__ == "__main__":
-    create_produto("X-Bacon",12.99,"Tradicionais")
-
-    read_produtos(1,5)
+    create_produto("X-Bacon", 12.90, "Tradicionais")
+    produtos = read_produtos(1, 5)
+    for produto in produtos:
+        print(produto)
