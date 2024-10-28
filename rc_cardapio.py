ecdd1f7bb812d027f37f189b304d4f0bb42e59bd
Eduardo Lemos <eduardo.lemosinc@gmail.com>
2024-10-27 22:17:46 -0300

    creating endpoints for API

--- rc_cardapio.py
+++ rc_cardapio.py
@@ -1,4 +1,6 @@
+from flask import Flask, request, jsonify
 import mysql.connector
+
 config = {
     'database': 'skipcr49_rc_cardapio',
     'user': 'skipcr49_junior',
@@ -7,16 +9,38 @@ config = {
     'port': '3306'
 }
 
-def create_produto (prod_name,prod_price,categoria):
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "hello"
+
+#create new product
+@app.route('/products',methods=['POST'])
+def create_produto ():
+
+    data = request.get_json()           #request reads the context of the http request
+    prod_name = data.get('prod_name')
+    prod_price = data.get('prod_price')
+    categoria = data.get('categoria')
+
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
     cursor.execute("INSERT INTO rc_cardapio(prod_name, prod_price, categoria) VALUES(%s,%s,%s)",(prod_name,prod_price,categoria))
     connection.commit()
     cursor.close()
     connection.close()
-    print("Produto inserido com sucesso.")
+    
+    return jsonify({"messege": "Produto inserido"}), 201
 
-def read_produtos(pagina,limite):
+
+#read all products paginated 
+@app.route('/products',methods=['GET'])
+def read_produtos():
+
+    pagina = int (request.args.get('pagina',1))
+    limite = int (request.args.get('limite',10))
+
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
     offset = (pagina - 1) * limite
@@ -26,25 +50,40 @@ def read_produtos(pagina,limite):
     connection.close()
     return rc_cardapio
 
-def update_produto(id, prod_name,prod_price,categoria):
+
+
+#updates a product
+@app.route('/products/<int:id>', methods=['PUT'])
+def update_produto(id):
+
+    data = request.get_json()           #request reads the context of the http request
+    prod_name = data.get('prod_name')
+    prod_price = data.get('prod_price')
+    categoria = data.get('categoria')
+
+
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
     cursor.execute("UPDATE rc_cardapio SET prod_name = %s, prod_price = %s, categoria = %s WHERE id = %s", (prod_name, prod_price, categoria,id))
     connection.commit()
     cursor.close()
     connection.close()
-    print("Produto atualizado com sucesso.")
+    
+    return jsonify({"messege": "Produto alterado"}), 200
 
+
+
+@app.route('/products/<int:id>', methods=['DELETE'])
 def delete_produto(id):
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
-    cursor.execute("DELETE FROM rc_cardapio WHERE id = %s", (id))
+    cursor.execute("DELETE FROM rc_cardapio WHERE id = %s", (id,))
     connection.commit()
     connection.close()
-    print("Produto deletado com sucesso.")
+    
+    return jsonify({"messege": "Produto deletado"}), 204
+
+
 
 if __name__ == "__main__":
-    create_produto("X-Bacon", 12.90, "Tradicionais")
-    produtos = read_produtos(1, 5)
-    for produto in produtos:
-        print(produto)
+    app.run(debug=True, host='0.0.0.0', port=5000)
